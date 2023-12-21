0508cf5ebd55bdca044e4fef4954ff9c0cf7c593
tohrxyz <tomas.hrib@proton.me>
2023-12-21 17:10:17 +0100

    refactor: restructure

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function HomePage() {
+  return (
+    <View className='flex mt-96'>
+      <Text className="text-5xl">Homepage</Text>
+    </View>
+  );
+}
